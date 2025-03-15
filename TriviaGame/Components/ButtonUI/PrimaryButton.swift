c4750f5c71a4210cae05e75fdd13a52ea99f41e9
rajnibanduni <Banduni.sharma@123>
2025-03-15 15:12:14 +0530

    App Iocns or Button Design done

---
+++ TriviaGame/Components/ButtonUI/PrimaryButton.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryButton.swift
+//  TriviaGame
+//
+//  Created by Rajni Banduni on 15/03/25.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PrimaryButton()
+}
